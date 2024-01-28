1d2c725576d7330fa0e81305e0f4a21cb736638d
Zioun <jionkhan0@gmail.com>
2024-01-28 08:30:48 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+const studentID = [10,12,14,16,19,20];
+console.log(studentID.length);
+console.log(studentID[2]);
+
+let StId = 10;
+StId = 50;
+console.log(StId);
+
+studentID[1] = 15;
+console.log(studentID);
+
+
