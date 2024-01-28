5e1fda75144909b082ce0f1351fdb2aa7d6131da
Zioun <jionkhan0@gmail.com>
2024-01-29 00:33:24 +0600

    added

---
+++ arrayJoin.js
@@ -0,0 +1,7 @@
+var alphabet = ["A","B","C","D","E","F","G"];
+// object
+console.log(typeof(alphabet));
+// string
+console.log(typeof(alphabet.join()));
+// custom edit
+console.log(alphabet.join(" + "));
